92d8fc716da85328422cd7ec60f71d8390ee2b2c
Arseniy Petrenko <arsfix2004@gmail.com>
2025-11-26 12:58:07 +0200

    start develop used cards logic

--- bj-counter.py
+++ bj-counter.py
@@ -5,6 +5,32 @@ your_card_1, your_card_2 = input("Введіть свої карти через 
 #Мапа карт
 card_map = {'K': 10, 'Q': 10, 'J': 10, 'A': 11}
 
+#Контроль карт, які вже були використані в грі
+used_cards = {
+    "2": 0,
+    "3": 0,
+    "4": 0,
+    "5": 0,
+    "6": 0,
+    "7": 0,
+    "8": 0,
+    "9": 0,
+    "10": 0,
+    "J": 0,   # Валет (J)
+    "Q": 0,   # Дама (Q)
+    "K": 0,   # Король (K)
+    "A": 0    # Туз (A)
+}
+
+def used_cards_counter(*cards):
+    for card in cards:
+        cards_upper = card.upper()
+        if cards_upper in used_cards:
+            used_cards[cards_upper] += 1
+    return None
+used_cards_counter(first_card_croupier, your_card_1, your_card_2)
+print("Список карт, які були використані", used_cards)
+
 # Буде конвертувати змінні в int
 def value_converter(*cards) -> list[int]:
     cards_list = []
